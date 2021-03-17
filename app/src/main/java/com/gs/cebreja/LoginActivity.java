c8465ea9355a29d43abf5b12e27ecccdd33dff26
Saulo de Oliveira Junior <mroluas@gmail.com>
2021-03-16 21:14:55 -0300

    New transitions and new input text
    New transitions and new input text

---
+++ app/src/main/java/com/gs/cebreja/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.gs.cebreja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+    }
+}
